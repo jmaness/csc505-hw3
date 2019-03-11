d89ac13c8e21f2784fcd3c03471542a62cad9672
Jeremy Maness <jwmaness@gmail.com>
2019-03-11 19:29:26 -0400

    Add basic script to run tests.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Overview
+This repository consists of all work for the 2019 CSC 505 HW3 assignment.
+
+# How to compile
+```
+javac *.java
+```
+
+# How to run
+```
+java naive < input-1.txt
+java greedy < input-1.txt
+java dp < input-1.txt
+```
+
+# How to execute tests
+```
+./run-tests.sh
+```
---
+++ run-tests.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+INPUT_FILES=("input-1.txt" "input-2.txt" "input-3.txt" "input-4.txt" "input-5.txt")
+ALGORITHMS=("naive" "greedy" "dp")
+EXPECTED_ANSWERS=(1472 672 284 438000 3630000 438000 3630000 3630000 438000 20375128344 7053967329 51646670 389013761244546 98296322558 98296322558)
+
+for i in ${!INPUT_FILES[@]}; do
+  for j in ${!ALGORITHMS[@]}; do
+    COMMAND="java ${ALGORITHMS[j]} < ${INPUT_FILES[i]}"
+    echo ${COMMAND}
+    RESULT=$(eval ${COMMAND})
+    EXPECTED_RESULT=${EXPECTED_ANSWERS[i*3+j]}
+
+    if [[ "$RESULT" != "${EXPECTED_RESULT}" ]]; then
+      echo "FAILED: ${ALGORITHMS[j]} ${INPUT_FILES[i]} - expected: ${EXPECTED_RESULT}, actual: $RESULT"
+    else
+      echo "PASSED"
+    fi
+  done
+done
